7ff632b86d3a7397ab279805588af14a05c84cbc
Mesut Yılmaz <im.mesut.yilmaz@gmail.com>
2023-11-05 20:45:59 +0300

    Initial Commit

---
+++ Shaky/ShakyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakyApp.swift
+//  Shaky
+//
+//  Created by Mesut Yılmaz on 5.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShakyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
